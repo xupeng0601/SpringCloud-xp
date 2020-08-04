e47b427c18db2657eb3e20797cd8da28397db25c
xupeng <fatiaoyezi@126.com>
2020-08-04 16:02:37 +0800

    父工程目录架构搭建

---
+++ cloud-provider-payment8001/src/main/java/org/xupeng/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package org.xupeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xupeng
+ * @Desc
+ * @date 2020/8/4 13:28
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
