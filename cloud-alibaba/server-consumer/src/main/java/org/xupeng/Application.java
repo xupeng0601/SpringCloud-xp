cb6b2489c6b9ba257e8670f84e2a636e67dea774
xucaoqing <fatiaoyezi@126.com>
2020-12-21 16:00:25 +0800

    整理项目结构

---
+++ cloud-alibaba/server-consumer/src/main/java/org/xupeng/Application.java
@@ -0,0 +1,46 @@
+package org.xupeng;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient  //开启服务注册发现功能
+@EnableFeignClients    //表明该服务使用Openfeign
+@MapperScan("org.xupeng.mapper")
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 注入RestTemplate调用server-provider服务;
+     * LoadBalanced注解表示如果被调用的服务有多个节点，则默认采用轮询调用，实现负载均衡
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @GetMapping("/consumer")
+    public String test() {
+        //url拼写规则:  传输协议://服务名/访问路径
+        return restTemplate.getForObject("http://SERVER-PROVIDER/hello",String.class);
+    }
+
+}
--- cloud-alibaba/server-provider/src/main/java/org/xupeng/Application.java
+++ cloud-alibaba/server-provider/src/main/java/org/xupeng/Application.java
@@ -5,17 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
-@SpringBootApplication
-@EnableFeignClients
 @MapperScan("org.xupeng.mapper")
+@EnableFeignClients
+@EnableDiscoveryClient  //能够让注册中心能够发现，扫描到改服务
+@SpringBootApplication
+@RestController
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/hello")
+    public String helloNacos(){
+        return "Hello，Nacos！";
+    }
 
 }
 
